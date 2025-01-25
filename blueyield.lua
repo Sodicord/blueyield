bda8235d40325284f185f05c59c9fea562bdc2ba
Sodi <uraniusrevive@gmail.com>
2025-01-24 19:36:53 -0800

    put as inf yield logo

--- blueyield.lua
+++ blueyield.lua
@@ -607,7 +607,7 @@ Logo.BackgroundTransparency = 1
 Logo.BorderSizePixel = 0
 Logo.Position = UDim2.new(0, 125, 0, 127)
 Logo.Size = UDim2.new(0, 10, 0, 10)
-Logo.Image = game:HttpGet("https://files.catbox.moe/tugtkj.png")
+Logo.Image = "rbxassetid://1352543873"
 Logo.ImageTransparency = 0
 Logo.ZIndex = 10
 
